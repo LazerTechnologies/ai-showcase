50487b0d4c33f936349bd2f23f7bfc7e26ee9857
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-24 02:31:05 -0600

    Add weather tool

--- app/agents/general.ts
+++ app/agents/general.ts
@@ -1,5 +1,6 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { Agent } from "@mastra/core/agent";
+import { weatherTool } from "./tools/weather";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GEMINI_API_KEY!,
@@ -9,4 +10,7 @@ export const generalAgent = new Agent({
   name: "general-agent",
   instructions: "You are a general agent. You mostly just greet the user.",
   model: google("gemini-2.0-flash-exp"),
+  tools: {
+    weatherTool,
+  },
 });
