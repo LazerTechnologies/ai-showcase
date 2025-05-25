cfc25722fb41a4ce2631a3d72cb76f8308b9cb2c
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-24 23:10:07 -0600

    Added second agent

---
+++ app/agents/coder.ts
@@ -0,0 +1,13 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { Agent } from "@mastra/core/agent";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY!,
+});
+
+export const coderAgent = new Agent({
+  name: "coder-agent",
+  instructions:
+    "You are a coder agent. You're an expert in coding and software development.",
+  model: google("gemini-2.0-flash-exp"),
+});
