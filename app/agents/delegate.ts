f455a87eecd6c69e0b1fc9b7da341d3c7d5cc973
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-26 02:32:45 -0600

    No delegate memory

--- app/agents/delegate.ts
+++ app/agents/delegate.ts
@@ -1,7 +1,5 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { Agent } from "@mastra/core/agent";
-import { Memory } from "@mastra/memory";
-import { LibSQLStore } from "@mastra/libsql";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GEMINI_API_KEY!,
@@ -9,13 +7,8 @@ const google = createGoogleGenerativeAI({
 
 export const delegateAgent = new Agent({
   name: "delegate-agent",
-  instructions: `You are a general-purpose agent. You don't have any tools, or produce output yourself, but you can delegate to other agents.
+  instructions: `You don't have any tools, or produce output yourself, but you can delegate to other agents via your toolset.
     Try not to ask clarifier questions unless absolutely necessary.
     When passing on work that other agents have done, make sure to credit their work.`,
   model: google("gemini-2.0-flash-exp"),
-  memory: new Memory({
-    storage: new LibSQLStore({
-      url: "file:../../memory.db",
-    }),
-  }),
 });
