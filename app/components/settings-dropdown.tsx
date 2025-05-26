d2681f1ce87d39c5e0677b2cfdea650af36a9174
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-26 02:06:30 -0600

    Constants

--- app/components/settings-dropdown.tsx
+++ app/components/settings-dropdown.tsx
@@ -26,9 +26,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { toast } from "sonner";
-
-export const USER_ID_STORAGE_KEY = "userId";
-export const THREAD_ID_STORAGE_KEY = "threadId";
+import {
+  USER_ID_STORAGE_KEY,
+  THREAD_ID_STORAGE_KEY,
+} from "@/app/constants/local-storage";
 
 const settingsSchema = z.object({
   userId: z.string().min(1, "User ID is required"),
---
+++ app/constants/local-storage.ts
@@ -0,0 +1,2 @@
+export const USER_ID_STORAGE_KEY = "userId";
+export const THREAD_ID_STORAGE_KEY = "threadId";
