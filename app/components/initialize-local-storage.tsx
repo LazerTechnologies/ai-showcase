de16507cac2ea5b6e0d231a6e95df672c09aeeee
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-27 09:09:50 -0600

    Use nanoid to initialize settings

---
+++ app/components/initialize-local-storage.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { nanoid } from "nanoid";
+import {
+  THREAD_ID_STORAGE_KEY,
+  USER_ID_STORAGE_KEY,
+} from "../constants/local-storage";
+
+export const InitializeLocalStorage = () => {
+  useEffect(() => {
+    if (localStorage.getItem(USER_ID_STORAGE_KEY) === null) {
+      localStorage.setItem(USER_ID_STORAGE_KEY, nanoid());
+    }
+    if (localStorage.getItem(THREAD_ID_STORAGE_KEY) === null) {
+      localStorage.setItem(THREAD_ID_STORAGE_KEY, nanoid());
+    }
+  }, []);
+
+  return null;
+};
