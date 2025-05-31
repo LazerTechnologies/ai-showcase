b602f81ac71a8a55fca7f3e3693b71b2376a998d
Joshua Saunders <jk.saunders@hotmail.com>
2025-05-31 08:26:32 -0600

    Button always has cursor-pointer unless disabled

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -50,7 +50,9 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, className }), {
+        "cursor-pointer": !props.disabled,
+      })}
       {...props}
     />
   );
