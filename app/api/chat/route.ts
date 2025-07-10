212268683871fb5974dcdfedc3b141abd160ddc7
Joshua Saunders <jk.saunders@hotmail.com>
2025-07-10 14:38:39 -0600

    Put back UserService, remove persistence

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,18 @@
 import { generalAgent } from "./general-agent";
+import { UserService } from "../../../services/user";
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-  // const user = await UserService.createIfNotExists(userId);
+  const { messages, userId, threadId } = await req.json();
+  const user = await UserService.createIfNotExists(userId);
+  console.log("user", user);
+  console.log("threadId", threadId);
 
-  const generalStream = await generalAgent.stream(messages, {});
+  const generalStream = await generalAgent.stream(messages, {
+    // resourceId: user.id,
+    // threadId,
+  });
 
   return generalStream.toDataStreamResponse();
 }
